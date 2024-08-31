293f0126244c80151a35719de93b78593661567b
MagomedovMurad20 <magdev09@gmail.com>
2024-08-31 18:15:17 +0300

    #add написал получение и вывод юзеров на фронтенд

--- bills/src/App.vue
+++ bills/src/App.vue
@@ -1,11 +1,28 @@
+
 <template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
   <div id="app">
     <UserList />
   </div>
 </template>
 
+
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { RouterLink, RouterView } from 'vue-router'
 import UserList from './components/UserList.vue';
 
 export default defineComponent({
